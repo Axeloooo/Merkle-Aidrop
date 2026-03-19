6a87b1db8376ee4bb076f389abd3260ad951b0b2
Axel Sanchez <axelshz@gmail.com>
2026-03-19 04:38:58 -0600

    Initial commit

---
+++ src/BagelToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BagelToken is ERC20, Ownable {
+    constructor() ERC20("BagelToken", "BAGEL") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
